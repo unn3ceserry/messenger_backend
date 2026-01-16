62b189b3ed295fc9299b9ce2cb0aa98db8a86abc
unn3ceserry <staak118@gmail.com>
2026-01-16 21:08:36 +0200

    feat: transformer when '' in password to undefined

--- src/modules/auth/account/dto/chnage-email.dto.ts
+++ src/modules/auth/account/dto/chnage-email.dto.ts
@@ -1,12 +1,24 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class ChangeEmailDto {
-  @IsNotEmpty({ message: 'Новый адрес электронной почты обязателен для заполнения.' })
+  @IsNotEmpty({
+    message: 'Новый адрес электронной почты обязателен для заполнения.',
+  })
   @IsEmail({}, { message: 'Введите корректный адрес электронной почты.' })
   newEmail: string;
 
   @IsOptional()
   @IsString({ message: 'Пароль для облака должен быть строкой.' })
-  @MinLength(6, { message: 'Пароль для облака должен содержать минимум 6 символов.' })
+  @Transform(({ value }) => (value === '' ? undefined : value))
+  @MinLength(6, {
+    message: 'Пароль для облака должен содержать минимум 6 символов.',
+  })
   cloudPassword?: string;
 }
